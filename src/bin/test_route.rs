48c2a409a4472c377654adbcf7534d072fd7ca93
chandler <chandler0149@gmail.com>
2026-06-26 21:25:46 +0800

    bugfix

--- src/bin/test_route.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-    println!("RTF_LLINFO = {:#x}", libc::RTF_LLINFO);
-    println!("RTF_UP = {:#x}", libc::RTF_UP);
-    println!("RTF_GATEWAY = {:#x}", libc::RTF_GATEWAY);
-    println!("RTF_HOST = {:#x}", libc::RTF_HOST);
-}
